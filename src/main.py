c71bd2623e00b34c8cbc41d8ed8034a97396da66
Andrew Boggs <166353465+AndrewBoggs55@users.noreply.github.com>
2025-12-30 01:51:57 -0600

    Bot function responding to messages

--- src/main.py
+++ src/main.py
@@ -14,9 +14,11 @@ class Client(discord.Client):  # Look into classes
     async def on_ready(self):  # on_ready is predefined function in discord.py
         print(f'Logged on as {self.user}!')  # Log on as the name as the bot
 
+    async def on_message(self, message):  # Predefined, called when mesg sent in server
+        print(f'Mesage from {message.author}: {message.content}')  # prints msg from server into term
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('TOKEN')
+client.run(TOKEN)
