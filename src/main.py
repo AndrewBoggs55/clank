b14ad7c5a37e47533e2b426541713e5c69b41f11
Andrew Boggs <166353465+AndrewBoggs55@users.noreply.github.com>
2025-12-30 23:33:35 -0600

    Adding event handling basics

--- src/main.py
+++ src/main.py
@@ -14,8 +14,15 @@ class Client(discord.Client):  # Look into classes
     async def on_ready(self):  # on_ready is predefined function in discord.py
         print(f'Logged on as {self.user}!')  # Log on as the name as the bot
 
-    async def on_message(self, message):  # Predefined, called when mesg sent in server
-        print(f'Mesage from {message.author}: {message.content}')  # prints msg from server into term
+    async def on_message(self, message):  # Predefined, mesg sent in server
+        if message.author == self.user:
+            return  # Bot can't reply to itself
+        if message.content.startswith('hello'):
+            await message.channel.send(f'Hi there {message.author}')
+
+    async def on_reaction_add(self, reaction, user):
+        await reaction.message.channel.send('You reacted')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
